15b64d3a6276d3f6d7cba49de5b03dd5ebbd130e
Ritheshwaran A <linkwithrithesh@gmail.com>
2026-07-06 18:26:46 +0530

    Update SCRIPT_URL to new Google script endpoint

--- firmware/config.h
+++ firmware/config.h
@@ -1,6 +1,6 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define SCRIPT_URL "https://script.google.com/macros/s/AKfycby5mf5JdY48SNHErOodud17jJ759HqH-53_7gOoRioiw6_j4zem8qVDu-FHhyOmC9F6/exec";
+#define SCRIPT_URL "https://script.google.com/macros/s/AKfycbyCOObCbxigw3jedSE9F8EHuoZLbMODp_AWbCio0SLO7qWmXY9rYJwu3sLPOyCvKDb8/exec";
 
 #endif
